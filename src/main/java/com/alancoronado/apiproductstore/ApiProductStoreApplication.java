f33d567daf88aeac4f72ba04fb02b0cfade064b7
Alan Coronado <alancrd16@gmail.com>
2023-12-22 23:43:26 -0700

    directory structure

---
+++ src/main/java/com/alancoronado/apiproductstore/ApiProductStoreApplication.java
@@ -0,0 +1,13 @@
+package com.alancoronado.apiproductstore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiProductStoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiProductStoreApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/alancoronado/apiproductstore/ApiProductStoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alancoronado.apiproductstore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiProductStoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
